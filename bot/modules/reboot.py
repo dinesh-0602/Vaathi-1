eff394421658f195119107b3413bfe8a6c24aff6
dinesh-0602 <rainadinesh94@gmail.com>
2021-09-11 12:19:56 +0000

    typo

--- bot/modules/reboot.py
+++ bot/modules/reboot.py
@@ -6,7 +6,7 @@ from bot import app, OWNER_ID, SUDO_USERS, bot
 from bot.helper import check_heroku
 
 
-@app.on_message(filters.command(['reboot', f'reboot@{bot.username}']) & filters.user(OWNER_ID) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(['reboot', f'reboot@{bot.username}']) & filters.user(OWNER_ID))
 @check_heroku
 async def gib_restart(client, message, hap):
     msg_ = await message.reply_text("**[HEROKU] - Restarting**")
