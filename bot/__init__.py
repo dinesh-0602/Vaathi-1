4558436c17324068ef0198132c7c5a2ffa93f08e
dinesh-0602 <rainadinesh94@gmail.com>
2021-09-11 12:16:06 +0000

    lol

--- bot/__init__.py
+++ bot/__init__.py
@@ -222,6 +222,15 @@ else:
     MEGA_USERNAME = None
     MEGA_PASSWORD = None
 try:
+    HEROKU_API_KEY = getConfig('HEROKU_API_KEY')
+    HEROKU_APP_NAME = getConfig('HEROKU_APP_NAME')
+    if len(HEROKU_API_KEY) == 0 or len(HEROKU_APP_NAME) == 0:
+        HEROKU_API_KEY = None
+        HEROKU_APP_NAME = None
+except KeyError:
+    HEROKU_API_KEY = None
+    HEROKU_APP_NAME = None
+try:
     INDEX_URL = getConfig("INDEX_URL")
     if len(INDEX_URL) == 0:
         INDEX_URL = None
